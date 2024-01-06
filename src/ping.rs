d4d17897c070df8628729a68830041e1868b7a1f
Tej Magar <tejmagar20510@gmail.com>
2024-01-06 11:21:22 +0545

    added ping

--- src/ping.rs
+++ src/ping.rs
@@ -1,20 +1,20 @@
 use std::io::Write;
 use std::net::TcpStream;
 use std::sync::{Arc, Mutex, RwLock, RwLockWriteGuard};
-use std::thread::{self, sleep};
+use std::thread::{self, JoinHandle, sleep};
 use std::time::Duration;
 use crate::protoc::encoder::build_raw_bytes;
 use crate::protoc::{File, StatusCode};
 
-pub fn ping(mut tcp_stream: Arc<RwLock<TcpStream>>, sleep_duration: Duration) {
+pub fn ping(mut tcp_stream: Arc<RwLock<TcpStream>>, sleep_duration: Duration) -> JoinHandle<()> {
     let files: Vec<&File> = Vec::new();
     let message: Vec<u8> = Vec::new();
     let ping_bytes = build_raw_bytes(1, StatusCode::Ping as u8, 1, &files, &message);
 
-    let thread = thread::spawn(move || {
-        sleep(sleep_duration);
-        tcp_stream.write().unwrap().write_all(&ping_bytes).unwrap();
+    return thread::spawn(move || {
+        loop {
+            sleep(sleep_duration);
+            tcp_stream.write().unwrap().write_all(&ping_bytes).unwrap();
+        }
     });
-
-    thread.join().unwrap();
 }
